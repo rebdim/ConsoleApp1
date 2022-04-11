fd37a2b93b4f4d9379bf69522c96e0da86ff0162
wsr7 <wsr7@college.edu>
2022-04-11 11:44:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionLambdas1
+{
+    public delegate bool CompareDelegate(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompareDelegate cD = (x, y) => x == y;
+            bool result = cD(9, 10);
+            Console.WriteLine(result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
